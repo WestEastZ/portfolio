801aecc94b3926ff15c02796bb32785f2345ea93
WestEastZ <85664676+WestEastZ@users.noreply.github.com>
2022-03-15 20:07:34 +0900

    JS : scroll_into_view

---
+++ tap_btn_move.js
@@ -0,0 +1,24 @@
+const navbarMenu = document.querySelector(".navbar__menu");
+const homeContact = document.querySelector(".home__button");
+
+// scrollIntoView function //
+function scrollIntoView(selection) {
+  const scrollTO = document.querySelector(selection);
+  scrollTO.scrollIntoView({ behavior: "smooth" });
+}
+
+// nav scroll //
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+// home contact scroll //
+homeContact.addEventListener("click", (event) => {
+  scrollIntoView("#contact");
+});
