935a63f350230d3128e6bb713a25aea4ebc7dd69
WestEastZ <85664676+WestEastZ@users.noreply.github.com>
2022-03-15 23:55:34 +0900

    JS : work__nav

--- work.js
+++ work.js
@@ -2,6 +2,7 @@ const workBtnContainer = document.querySelector(".work__categories");
 const projectCOntainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
 
+// clilck work //
 workBtnContainer.addEventListener("click", (event) => {
   const filter =
     event.target.dataset.filter || event.target.parentNode.dataset.filter;
@@ -9,6 +10,14 @@ workBtnContainer.addEventListener("click", (event) => {
     return;
   }
 
+  // remove selection //
+  const active = document.querySelector(".category__btn.seleted");
+  active.classList.remove("seleted");
+  const target =
+    event.target.nodeName === "BUTTON" ? event.target : event.target.parentNode;
+  target.classList.add("seleted");
+
+  // project remove //
   projects.forEach((project) => {
     if (filter === "all" || filter === project.dataset.type) {
       project.classList.remove("invisible");
